695bb66ddfdf443f372e4d3e83e97540150c9b68
Henry Gabriel <63942305+HenryGabriel-2407@users.noreply.github.com>
2024-04-05 15:11:25 -0300

    Create livros.py

---
+++ livros.py
@@ -0,0 +1,19 @@
+"""
+Primeira entrega: 19/04
+Segunda entrega: 20/06
+Individual
+Sem apresentação
+
+Classes Principais:
+    Livro: Uma classe para representar um livro, com atributos como título, autor, ISBN, gênero, etc.
+    Biblioteca: Uma classe para representar a biblioteca, que contém uma lista de livros e métodos para adicionar, remover e pesquisar livros.
+
+Funcionalidades:
+    Adicionar um novo livro à biblioteca.
+    Remover um livro da biblioteca.
+    Editar um livro da biblioteca. (um CRUD)
+    Pesquisar um livro por título, autor, ISBN, ou gênero.
+    Exibir todos os livros disponíveis na biblioteca.
+    Permitir o empréstimo e devolução de livros, com controle de disponibilidade.
+
+"""
