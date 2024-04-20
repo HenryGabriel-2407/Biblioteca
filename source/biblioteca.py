728c645d03140b3c735109ef723010804010809e
Henry Gabriel <63942305+HenryGabriel-2407@users.noreply.github.com>
2024-04-19 22:00:37 -0300

    Metade do caminho....

--- source/biblioteca.py
+++ source/biblioteca.py
@@ -17,8 +17,9 @@ class Biblioteca():
         #Se o livro não existe na biblioteca
         sql = f"INSERT INTO `livro` (`titulo`, `autor`, `genero`, `ano`, `avaliacao`, `quantidade`) VALUES ('{livro._Livro__titulo}', '{livro._Livro__autor}', '{livro._Livro__genero}', {livro._Livro__ano}, {livro._Livro__avaliacao}, {livro._Livro__quantidade})"
         cursor.execute(sql)
-        print("Livro inserido no banco de dados com sucesso.")
+        print("Livro inserido no banco de dados com sucesso. \n {'#' * 40}")
         conexao.commit()
+        print("#" * 40)
 
 
     def listar_livros(self) -> None: #STATUS: FUNCIONANDO
@@ -33,6 +34,7 @@ class Biblioteca():
             print(f"\tAno: {ano}")
             print(f"\tAvaliação: {estrelas}")
             print(f"\tQuantidade: {quantidade}\n")
+        print("#" * 40)
 
 
     def livros_disponiveis(self) -> None: #STATUS: FUNCIONANDO
@@ -47,6 +49,7 @@ class Biblioteca():
             print(f"\tAno: {ano}")
             print(f"\tAvaliação: {estrelas}")
             print(f"\tQuantidade: {quantidade}\n")
+        print("#" * 40)
 
 
     def remover_livro(self) -> None: #STATUS: FUNCIONANDO
@@ -54,9 +57,11 @@ class Biblioteca():
         try:
             sql = f"DELETE FROM livro WHERE titulo = '{nome_livro}'"
             cursor.execute(sql)
+            print(f"Livro {nome_livro} foi excluído com sucesso!")
             conexao.commit()
         except:
             print("Erro, tente novamente...")
+        print("#" * 40)
 
 
     def pesquisar_livro(self) -> None: #STATUS: FUNCIONANDO
@@ -86,6 +91,7 @@ class Biblioteca():
                 print(f"\tQuantidade: {quantidade}\n")
         except Exception as e:
             print("Erro: ", e)
+        print("#" * 40)
 
 
     def editar_livro(self) -> None: #STATUS: FUNCIONANDO
@@ -109,15 +115,17 @@ class Biblioteca():
             quantidade = int(input("Digite a quantidade: "))
             sql = f"UPDATE livro SET titulo = '{titulo_modificado}', autor = '{autor}', genero = '{genero}', ano = {ano}, avaliacao = {avaliacao}, quantidade = {quantidade} WHERE titulo = '{pesquisar_nome}'"
             cursor.execute(sql)
-            print("Modificado!")
+            print("Modificado com sucesso!")
             conexao.commit()
         except:
             print("Erro, tente novamente...")
+        print("#" * 40)
     
+
     def emprestimo_livro(self):
         pesquisar_nome = str(input("Digite o título do livro: "))
         try:
-            sql = f"SELECT * FROM livro WHERE titulo = '{pesquisar_nome}'"
+            sql = f"SELECT titulo, autor, genero, ano, avaliacao, quantidade FROM livro WHERE titulo = '{pesquisar_nome}'"
             cursor.execute(sql)
             for (titulo, autor, genero, ano, avaliacao, quantidade) in cursor:
                 estrelas = str('\U0001F31F' * int(avaliacao))
@@ -128,9 +136,10 @@ class Biblioteca():
                 print(f"\tAvaliação: {estrelas}")
                 print(f"\tQuantidade: {quantidade}")
             levar_ou_nao = str(input("Deseja levar? [Y/N]: "))
-            if levar_ou_nao == 'Y' and quantidade > 1:
-                sql = f"UPDATE livro SET quantidade = {quantidade - 1} WHERE titulo = '{pesquisar_nome}'"
+            if levar_ou_nao == 'Y' and int(quantidade) > 1:
+                sql = f"UPDATE livro SET quantidade = {int(quantidade) - 1} WHERE titulo = '{pesquisar_nome}'"
                 cursor.execute(sql)
+                print("Empréstimo feito com sucesso!")
                 conexao.commit()
             elif levar_ou_nao == "N":
                 print("OK!")
@@ -138,11 +147,19 @@ class Biblioteca():
                 print(f"Infelizmente não poderá levar o {pesquisar_nome}")
         except:
             print("Erro, tente novamente...")
+        print("#" * 40)
 
 
-    def devolucao_livro(self):
+    def devolucao_livro(self): #STATUS: FUNCIONANDO
         pesquisar_nome = str(input("Digite o título do livro: "))
         try:
             sql = f"SELECT quantidade FROM livro WHERE titulo = '{pesquisar_nome}'"
             cursor.execute(sql)
-
+            nova_quantidade = (cursor.fetchone()[0]) + 1
+            sql = f"UPDATE livro SET quantidade = {nova_quantidade} WHERE titulo = '{pesquisar_nome}'"
+            cursor.execute(sql)
+            print("Devolução feita com sucesso!")
+            conexao.commit() 
+        except:
+            print("Erro, tente novamente...")
+        print("#" * 40)
--- source/main.py
+++ source/main.py
@@ -1,18 +1,35 @@
 from livros import Livro
 from biblioteca import Biblioteca
 
-armazem = Biblioteca()
-'''
-titulo = str(input("Digite o título de livro: "))
-autor = str(input("Digite o nome do autor(a): "))
-genero = str(input("Digite o gênero: "))
-ano = int(input("Digite o ano: "))
-avaliacao = int(input("Digite a nota de avaliação [0, 10]: "))
-quantidade = int(input("Digite a quantidade: "))
-
-livro_novo = Livro(titulo, autor, genero, ano, avaliacao, quantidade)
-#armazem.remove_livro()
-armazem.add_livro(livro_novo)'''
-armazem.listar_livros()
-print("=" * 40)
-armazem.pesquisar_livro()
+biblioteca = Biblioteca()
+print("=======BIBLIOTECA UNIAVAN=======\n")
+while True:
+    escolha = int(input("[1]- Adicionar um livro \n[2]- Remover um livro \n[3]- Pesquisar \n[4]- Editar \n[5]- Listar todos os livros \n[6]- Listar os livros disponíveis \n[7]- Empréstimo \n[8]- Devolução \n[9]- Sair do Sistema \n--->"))
+    if escolha == 1:
+        titulo = str(input("Digite o título de livro: "))
+        autor = str(input("Digite o nome do autor(a): "))
+        genero = str(input("Digite o gênero: "))
+        #ISBN não está funcionando ainda, por isso, tente por nome completo do livro
+        ano = int(input("Digite o ano: "))
+        avaliacao = int(input("Digite a nota de avaliação [0, 10]: "))
+        quantidade = int(input("Digite a quantidade: "))
+        livro_novo = Livro(titulo, autor, genero, ano, avaliacao, quantidade)
+        biblioteca.add_livro(livro_novo)
+    elif escolha == 2:
+        biblioteca.remover_livro()
+    elif escolha == 3:
+        biblioteca.pesquisar_livro()
+    elif escolha == 4:
+        biblioteca.editar_livro()
+    elif escolha == 5:
+        biblioteca.listar_livros()
+    elif escolha == 6:
+        biblioteca.livros_disponiveis()
+    elif escolha == 7:
+        biblioteca.emprestimo_livro()
+    elif escolha == 8:
+        biblioteca.devolucao_livro()
+    elif escolha == 9:
+        break
+    else:
+        continue
